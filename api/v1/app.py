b706085debcd687faa7797b418a3bd02db53a15f
Come Le Sauter <come.l.s@hotmail.fr>
2022-09-16 13:29:03 +0200

    pycode

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,15 +12,18 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
 
+
 @app.errorhandler(404)
 def page_not_found(exception):
     """ Custom 404 error """
     return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == '__main__':
     app.debug = True
