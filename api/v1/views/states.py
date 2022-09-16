dd14890b33c5aade82415ca25a9d538f58f295dd
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-09-16 09:40:57 +0200

    task7

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" Method HTTP for States """
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
@@ -13,15 +14,16 @@ def get_all_states():
         all_states.append(state.to_dict())
     return jsonify(all_states)
 
+
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-def get_state(state_id):  # sourcery skip: assign-if-exp, reintroduce-else
+def get_state(state_id):
     """ Function that retrieves a State """
     state = storage.get(State, state_id)
-    if state is None:
-        return abort(404)
-    return jsonify(state.to_dict())
+    return abort(404) if state is None else jsonify(state.to_dict())
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state(state_id):
     """ Function that deletes a state """
     state = storage.get(State, state_id)
@@ -30,6 +32,7 @@ def delete_state(state_id):
     state.delete()
     storage.save()
     return jsonify({}), 200
+
 
 @app_views.route('/states/', methods=['POST'], strict_slashes=False)
 def post_state():
@@ -47,8 +50,9 @@ def post_state():
 
     return jsonify(new_state.to_dict()), 201
 
+
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
-def put_state(state_id):  # sourcery skip: assign-if-exp, reintroduce-else
+def put_state(state_id):
     """ Function that update a state """
     state = storage.get(State, state_id)
     if state is None:
@@ -65,4 +69,3 @@ def put_state(state_id):  # sourcery skip: assign-if-exp, reintroduce-else
     state.save()
 
     return jsonify(state.to_dict()), 200
-
